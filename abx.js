94e114a6fc011c386b8d1d17c764d878fa2c83ca
chomeyama <yoneyama.reo@g.sp.m.is.nagoya-u.ac.jp>
2021-07-09 02:05:08 +0900

    fixed and add commentout

--- abx.js
+++ abx.js
@@ -1,6 +1,6 @@
-Array.prototype.shuffle = function(){
+Array.prototype.shuffle = function () {
     var i = this.length;
-    while(i){
+    while (i) {
         var j = Math.floor(Math.random() * i);
         var t = this[--i];
         this[i] = this[j];
@@ -10,17 +10,17 @@ Array.prototype.shuffle = function(){
 }
 
 // invalid enter key
-function invalid_enter(){
-    if(window.event.keyCode == 13){
+function invalid_enter() {
+    if (window.event.keyCode == 13) {
         return false;
     }
 }
 
 // start experiment
-function start_experiment(){
+function start_experiment() {
     // get user name
     var name = document.getElementById("name").value.replace(" ", "_");
-    if(name == ""){
+    if (name == "") {
         alert("Please enter your name.");
         return false;
     }
@@ -28,72 +28,100 @@ function start_experiment(){
     // get setlist number
     var set_num = "0"
     var number = document.getElementsByName("set");
-    for(var i=0; i<number.length; i++){
-        if(number[i].checked){
+    for (var i = 0; i < number.length; i++) {
+        if (number[i].checked) {
             set_num = number[i].value;
         }
     }
 
-    if(set_num == "0"){
+    if (set_num == "0") {
         alert("Please press the setlist number button.");
         return false;
     }
 
     // convert display
-    Display()
+    Display();
 
-    // read filepath
-    var method0_list = wav_dir + "set" + set_num + "/world.list";
-    var method1_list = wav_dir + "set" + set_num + "/nsf.list";
-    var method2_list = wav_dir + "set" + set_num + "/qppwg.list";
-    var method3_list = wav_dir + "set" + set_num + "/usfgan.list";
-    method0 = loadText(method0_list);
-    method1 = loadText(method1_list);
-    method2 = loadText(method2_list);
-    method3 = loadText(method3_list);
+    /*
+        you have to customize this part
+    */
+    var method_list_path = [];
+    // the first one is the reference method
+    method_list_path.push(wav_dir + "set" + set_num + "/world.list");
+    // the following ones are methods you want to compare
+    method_list_path.push(wav_dir + "set" + set_num + "/nsf.list");
+    method_list_path.push(wav_dir + "set" + set_num + "/qppwg.list");
+    method_list_path.push(wav_dir + "set" + set_num + "/usfgan.list");
+    /*
+        end
+    */
+
+    file_list = makeFileList(method_list_path);
     outfile = name + "_set" + set_num + ".csv";
-    file_list = makeFileList()
-    console.log(file_list);
     scores = (new Array(file_list.length)).fill(0);
     eval = document.getElementsByName("eval");
-    init()
+    init();
 
 }
 
 // convert display
-function Display(){
+function Display() {
     document.getElementById("Display1").style.display = "none";
     document.getElementById("Display2").style.display = "block";
 }
 
 // load text file
-function loadText(filename){
+function loadText(filename) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", filename, false);
     xhr.send(null);
     var list = xhr.responseText.split(/\r\n|\r|\n/);
-    list.pop()
+    list.pop();
 
     return list;
 }
 
 // make file list
-function makeFileList(){
-    var files = Array((method1.length) * 6);
-    for(var i = 0; i < (method1.length); i++){
-        pairs = [[method0[i], method1[i], method2[i]], [method0[i], method2[i], method1[i]], 
-                 [method0[i], method2[i], method3[i]], [method0[i], method3[i], method2[i]], 
-                 [method0[i], method1[i], method3[i]], [method0[i], method3[i], method1[i]]]
-        for(var j = 0; j < 6; j++){
-            files[i * 6 + j] = pairs[j];
+function makeFileList(method_list_path) {
+    // prepare file list of all methods
+    var method = Array();
+    for (var i = 0; i < method_list_path.length; i++) {
+        // method[0] is the reference method
+        // method[1]~ are the methods you compare
+        method.push(loadText(method_list_path[i]));
+    }
+
+    var files = Array();
+    for (var i = 0; i < method[0].length; i++) {
+        // TODO: automate here
+        /*
+            you have to customize this part
+            if you don't want to compare 3 methods
+            you need to rewrite 'pairs'.
+            'pairs' consists of n_P_2 triplets, where
+            'n' is num of methods you want to compare.
+        */
+        pairs = [
+            [method[0][i], method[1][i], method[2][i]],
+            [method[0][i], method[2][i], method[1][i]],
+            [method[0][i], method[2][i], method[3][i]],
+            [method[0][i], method[3][i], method[2][i]],
+            [method[0][i], method[1][i], method[3][i]],
+            [method[0][i], method[3][i], method[1][i]],
+        ]
+        /*
+            end
+        */
+
+        for (var j = 0; j < pairs.length; j++) {
+            files.push(pairs[j]);
         }
     }
     files.shuffle();
-
     return files;
 }
 
-function setAudio(){
+function setAudio() {
     document.getElementById("page").textContent = "" + (n + 1) + "/" + scores.length;
 
     document.getElementById("audio_x").innerHTML = 'VoiceX:<br>'
@@ -112,76 +140,76 @@ function setAudio(){
         + '</audio>';
 }
 
-function init(){
+function init() {
     n = 0;
     setAudio();
     evalCheck();
     setButton();
 }
 
-function evalCheck(){
+function evalCheck() {
     const c = scores[n];
-    if((c <= 0) || (c > eval.length)){
-        for(var i = 0; i < eval.length; i++){
+    if ((c <= 0) || (c > eval.length)) {
+        for (var i = 0; i < eval.length; i++) {
             eval[i].checked = false;
         }
     }
-    else{
-        eval[c-1].checked = true;
+    else {
+        eval[c - 1].checked = true;
     }
 }
 
-function setButton(){
-    if(n == (scores.length - 1)){
-        document.getElementById("prev").disabled=false;
-        document.getElementById("next2").disabled=true;
-        document.getElementById("finish").disabled=true;
-        for(var i = 0; i < eval.length; i++){
-            if(eval[i].checked){
-                document.getElementById("finish").disabled=false;
+function setButton() {
+    if (n == (scores.length - 1)) {
+        document.getElementById("prev").disabled = false;
+        document.getElementById("next2").disabled = true;
+        document.getElementById("finish").disabled = true;
+        for (var i = 0; i < eval.length; i++) {
+            if (eval[i].checked) {
+                document.getElementById("finish").disabled = false;
                 break;
             }
         }
     }
-    else{
-        if(n == 0){
-            document.getElementById("prev").disabled=true;
+    else {
+        if (n == 0) {
+            document.getElementById("prev").disabled = true;
         }
-        else{
-            document.getElementById("prev").disabled=false;
+        else {
+            document.getElementById("prev").disabled = false;
         }
-        document.getElementById("next2").disabled=true;
-        document.getElementById("finish").disabled=true;
-        for(var i = 0; i < eval.length; i++){
-            if(eval[i].checked){
-                document.getElementById("next2").disabled=false;
+        document.getElementById("next2").disabled = true;
+        document.getElementById("finish").disabled = true;
+        for (var i = 0; i < eval.length; i++) {
+            if (eval[i].checked) {
+                document.getElementById("next2").disabled = false;
                 break;
             }
         }
     }
 }
 
-function evaluation(){
-    for(var i = 0; i < eval.length; i++){
-        if(eval[i].checked){
+function evaluation() {
+    for (var i = 0; i < eval.length; i++) {
+        if (eval[i].checked) {
             scores[n] = i + 1;
         }
     }
     setButton();
 }
 
-function exportCSV(){
+function exportCSV() {
     var csvData = "";
-    for(var i = 0; i < file_list.length; i++){
+    for (var i = 0; i < file_list.length; i++) {
         csvData += "" + file_list[i][1] + ","
-        + file_list[i][2] + ","
-        + scores[i] + "\r\n";
+            + file_list[i][2] + ","
+            + scores[i] + "\r\n";
     }
 
     const link = document.createElement("a");
     document.body.appendChild(link);
     link.style = "display:none";
-    const blob = new Blob([csvData], {type: "octet/stream"});
+    const blob = new Blob([csvData], { type: "octet/stream" });
     const url = window.URL.createObjectURL(blob);
     link.href = url;
     link.download = outfile;
@@ -190,26 +218,24 @@ function exportCSV(){
     link.parentNode.removeChild(link);
 }
 
-function next(){
+function next() {
     n++;
     setAudio();
     evalCheck();
     setButton();
 }
 
-function prev(){
+function prev() {
     n--;
     setAudio();
     evalCheck();
     setButton();
 }
 
-function finish(){
+function finish() {
     exportCSV();
 }
 
-
-// --------- 設定 --------- //
 
 // directory name
 const wav_dir = "wav/";
@@ -217,14 +243,11 @@ const wav_dir = "wav/";
 // invalid enter key
 document.onkeypress = invalid_enter();
 
-var method0;
-var method1;
-var method2;
-var method3;
+// global variables
 var outfile;
 var file_list;
 var scores;
 
-// ローカルで行う場合はloadText()は動作しないため
+// since loadText() doesn't work in local
 var n = 0;
 var eval = document.getElementsByName("eval");
